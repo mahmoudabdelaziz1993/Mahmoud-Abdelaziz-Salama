1c50fd964ddca1f5964fd08c029df7e5ef47b1c4
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2024-03-21 21:23:58 +0200

    fix  buld issues

--- next.config.js
+++ next.config.js
@@ -1,21 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'icons8.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.icons8.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'creazilla-store.fra1.digitaloceanspaces.com',
-            }
-        ],
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "icons8.com"
+      },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com"
+      },
+      {
+        protocol: "https",
+        hostname: "creazilla-store.fra1.digitaloceanspaces.com"
+      }
+    ]
+  },
+  webpack: config => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+      string_decoder: false,
+      dns: false,
+      net: false,
+      stream: false,
+      crypto: false,
+      tls: false
+    };
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
